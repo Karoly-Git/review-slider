5f6b47f96100b44d505ed96203b4f91a54feb488
Karoly <hornyal.mobile@gmail.com>
2022-12-14 09:45:03 +0000

    first commit

---
+++ reviews.js
@@ -0,0 +1,34 @@
+const reviews = [
+    {
+        stars: 5,
+        name: "Stephen Hawking",
+        review:
+            `“ Remember to look up at the stars and not down at your feet. Try to make sense of what you see and wonder about what makes the universe exist. Be curious. And however difficult life may seem, there is always something you can do and succeed at. ”`
+    },
+    {
+        stars: 5,
+        name: "Albert Einstein",
+        review:
+            `“ Two things are infinite: the universe and human stupidity; and I'm not sure about the universe. ”`
+    },
+    {
+        stars: 5,
+        name: "Michio Kaku",
+        review:
+            `“ The human brain has 100 billion neurons, each neuron connected to 10 thousand other neurons. Sitting on your shoulders is the most complicated object in the known universe. ”`
+    },
+    {
+        stars: 5,
+        name: "Brian Greene",
+        review:
+            `“ My best teachers were not the ones who knew all the answers, but those who were deeply excited by questions they couldn't answer. ”`
+    },
+    {
+        stars: 5,
+        name: "Niels Bohr",
+        review:
+            `“ An expert is a man who has made all the mistakes which can be made, in a narrow field. ”`
+    }
+];
+
+export default reviews;
